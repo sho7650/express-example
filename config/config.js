0e4ac73c70af71023689de25097588e34bc28a84
sho kisaragi <sho@oshiire.to>
2018-05-04 21:47:29 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,10 +8,6 @@ module.exports = {
     storage: ":memory:"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
+    "use_env_variable": "DATABASE_URL"
   }
 };
